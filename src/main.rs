ccfbbf3e5414d67539870e0f73756178188eed95
Alireza Soltani <40909549+Asncodes-80@users.noreply.github.com>
2022-04-10 11:42:23 +0430

    Add Vector maker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    file_picker("sample.txt");
+}
+
+pub fn file_picker(file_name: &str) {
+    // Reading file from the root
+    let file: String = fs::read_to_string(file_name).expect("Error in reading {} file!");
+    
+    // Printing the file content
+    let splitted_file_string: Vec<String> = file.split("\r").map(|w| w.to_string()).collect();
+    println!("{:?}", splitted_file_string);
+
+    let mut final_value: Vec<Vec<String>> = Vec::new();
+    for line in splitted_file_string {
+        // println!("{}", line);
+        let block_as_string: Vec<String> = line.split(" ").map(|w| w.to_string()).collect();
+        final_value.push(block_as_string);
+    }
+
+    print!("{:?}", final_value[0][1]);
 }
