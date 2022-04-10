a5eb1faebebdd964b22c60e95155fdebfe36d8e5
Alireza Soltani <40909549+Asncodes-80@users.noreply.github.com>
2022-04-10 14:28:57 +0430

    Add QuickSplit extention

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
 use std::fs;
 fn main() {
-    file_picker("sample.txt");
+    let file_content = file_picker("sample.txt");
+    println!("{:?}", file_content[0][3]);
 }
 
-pub fn file_picker(file_name: &str) {
+pub fn file_picker(file_name: &str) -> Vec<Vec<String>> {
     // Reading file from the root
-    let file: String = fs::read_to_string(file_name).expect("Error in reading {} file!");
-    
-    // Printing the file content
-    let splitted_file_string: Vec<String> = file.split("\r").map(|w| w.to_string()).collect();
-    println!("{:?}", splitted_file_string);
+    let file: String = fs::read_to_string(file_name)
+    .expect("Error in reading {} file!");
+
+    // Split array to level 1
+    let splitted_file_string: Vec<String> = file.split_to_vec("\r");
 
     let mut final_value: Vec<Vec<String>> = Vec::new();
     for line in splitted_file_string {
-        // println!("{}", line);
-        let block_as_string: Vec<String> = line.split(" ").map(|w| w.to_string()).collect();
+        let block_as_string: Vec<String> = line.split_to_vec(" ");
         final_value.push(block_as_string);
     }
 
-    print!("{:?}", final_value[0][1]);
+    return final_value;
+}
+
+trait QuickSplit { 
+    // Getting only Vec of Strings
+    fn split_to_vec(&self, separated_by: &'static str) -> Vec<String>;
+}
+
+impl QuickSplit for String {
+    fn split_to_vec(&self, separated_by: &'static str) -> Vec<String> {
+        return self.split(separated_by).map(|w| w.to_string()).collect();
+    }
 }
